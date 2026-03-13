0e09e38def228550d4c7934dc153f13be23eef49
Integral-ot-exp <148421773+Integral-ot-exp@users.noreply.github.com>
2026-03-13 08:32:52 +0300

    .

---
+++ Полезные файлы/VirF_Sound_Receiver/Скетчи/reader/reader.ino
@@ -0,0 +1,17 @@
+/* Спец проект youtube - канала VirF Live Productions
+Адрес канала www.youtube.com/c/VirFLiveProductions
+Группа посвященная каналу https://vk.com/virf_live_productions
+version 1.0 от 24.02.17
+*/
+#include <RF24.h>   
+#include <SPI.h>
+#include <RF24V.h>
+RF24 radio(7,8);    
+RF24V Sound(radio,0);
+void setup() {
+radio.begin();
+radio.setChannel(0x4b);
+radio.setDataRate(RF24_250KBPS); //250KBPS, 1MBPS, 2MBPS (250KBPS на nRF24L01 (без +) неработает)
+Sound.begin();
+Sound.reader();} //  - Включаем прием 
+void loop() {}
