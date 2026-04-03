dcedf275322b83694e7aafcee02a9820014a7084
Integral-ot-exp <148421773+Integral-ot-exp@users.noreply.github.com>
2026-04-03 17:54:30 +0300

    Изменил программы

--- Diplom_peredatchik/Diplom_peredatchik.ino
+++ Diplom_peredatchik/Diplom_peredatchik.ino
@@ -2,21 +2,77 @@
 #include "SPI.h"
 #include "nRF24L01.h"
 #include "RF24.h"
-char msg[6] = "hello";
-RF24 radio(43, 6, 26, 48, 38);
+#define SCK 48
+#define MISO 47
+#define MOSI 38
+#define CE 6
+#define CS 14
+
+RF24 radio(CE, CS);
+char msg[6] = "1";
+char msg2[6] = "0";
 uint64_t pope = 0xB3B4B5B6F1;
+
+void setup(void)
+{
+  //Serial.println(“Starting SPI”);
+  //Serial.flush();
+  SPI.begin(SCK, MISO, MOSI, CS);
+  /*Serial.println(“Started”);
+  Serial.flush();
+
+  while (!radio.begin(&SPI)) //активировать модуль
+  {
+    Serial.println(“Can’t find RF24”);
+    Serial.flush();
+    delay(1);
+  }*/
+
+  //Serial.println(“OK”);
+  Serial.begin(115200);
+  radio.begin();
+  radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+
+  radio.setChannel(2);
+  radio.setPayloadSize(7);
+  radio.setDataRate(RF24_250KBPS);
+  radio.openWritingPipe(pope);
+
+  radio.stopListening();          //This sets the module as transmitter
+
+}
+
+
+
+//RF24 radio(43, 6, 26, 48, 38);
+
 //RF24 radio(43, 6);
  
-void setup(void) {
+/*void setup(void) {
   Serial.begin(115200);
   radio.begin();
   radio.setChannel(2);
   radio.setPayloadSize(7);
   radio.setDataRate(RF24_250KBPS);
   radio.openWritingPipe(pope);
-}
-void loop(void) {
+}*/
+/*void loop(void) {
   //Serial.println("send ...");
   radio.write(msg, 6);
-  delay(3000);
+  delay(300);
+  radio.write(msg2, 6);
+  delay(100);
+}*/
+
+void loop()
+{
+  int counter = 0;
+char text[] = "1";
+char str[6];
+sprintf(str,"%s %d",text,counter);
+radio.write(&str, sizeof(str));  
+ 
+Serial.println(str);
+counter++;
+delay(300);
 }
--- Diplom_priemnik/Diplom_priemnik.ino
+++ Diplom_priemnik/Diplom_priemnik.ino
@@ -1,8 +1,16 @@
-#include <stdint.h>
+/*#include <stdint.h>
+//#include "main.h"
 #include "SPI.h"
 #include "nRF24L01.h"
 #include "RF24.h"
-RF24 radio(10,4);
+//RF24 radio(10,4);
+#define SCK 15
+#define MISO 16
+#define MOSI 17
+#define CE 31
+#define CS 14
+
+RF24 radio(CE, CS);
 const uint64_t pope = 0xb3b4b5b6f1;
 void setup(void){
  pinMode(PC13, OUTPUT);
@@ -17,16 +25,64 @@ void setup(void){
  
 void loop(void){
  if (radio.available()){  
-  digitalWrite(PC13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);
-  digitalWrite(PC13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);
+  //digitalWrite(PC13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  //delay(1000);
+  radio.read(msg, 6);
+  if (msg == "1")
+  {
+    digitalWrite(PC13, LOW);
+  }
+  else {
+    digitalWrite(PC13, HIGH);
+  }    // turn the LED off by making the voltage LOW
+  //delay(300);
  }
  else{
-  digitalWrite(PC13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(300);
+  //digitalWrite(PC13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  /*delay(300);
   digitalWrite(PC13, LOW);    // turn the LED off by making the voltage LOW
-  delay(300);
+  delay(1000);*/
   //Serial.println("No radio available");
- }
+// }
+//}*/
+
+//Arduino
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+ 
+RF24 radio(PB0, PA4); // CE, CSN on Blue Pill
+const uint64_t address = 0xB3B4B5B6F1;
+boolean button_state = 0;
+ 
+void setup() 
+{
+  Serial.begin(115200);
+  radio.begin();
+  //Serial.print("ADDRESS :");
+  radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
+  radio.setPALevel(RF24_PA_MIN);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
+  radio.setChannel(2);
+  radio.startListening();              //This sets the module as receiver
+}
+
+void loop()
+{
+  if (radio.available())              //Looking for the data.
+  {
+    Serial.println("Radio is sniffing");
+  
+    char text[6] = "";                 //Saving the incoming data
+    radio.read(&text, sizeof(text));    //Reading the data
+   //Serial.println(text);
+    if (text == "1")
+    {
+      digitalWrite(PC13, LOW);
+    }
+    if (text == "0")
+    {
+      digitalWrite(PC13, HIGH);
+
+    }
+  }
 }
